a12373bed757cf318c86f6dd39c2a8b30d4d4717
ruka_nurui <k020g1236@g.neec.ac.jp>
2023-11-17 10:11:46 +0900

    タイトルシーン追加

--- Scene/TitleScene.cpp
+++ Scene/TitleScene.cpp
@@ -16,10 +16,66 @@ void TitleScene::Initialize(DXCommon* dxcommon, Input* input, Audio* audio, Spri
     this->spriteCommon = spritecommon;
     this->Windows = windows;
 
+    //カメラ生成
+    camera = new Camera(this->input, this->Windows);
+    camera->Initialize(WindowsApp::window_width, WindowsApp::window_height, this->input);
+
+    //FBX関連
+    //デバイスをセット
+    FBXobj3d::SetDevice(dxCommon->GetDevice());
+    //カメラをセット
+    FBXobj3d::SetCamera(camera);
+    //FBXパイプライン生成
+    FBXobj3d::CreateGraphicsPipeline();
+
+    bamodel = FbxLoader::GetInstance()->LoadModelFromFile("ba");
+    nnmodel = FbxLoader::GetInstance()->LoadModelFromFile("nn");
+    zumodel = FbxLoader::GetInstance()->LoadModelFromFile("zu");
+    rimodel = FbxLoader::GetInstance()->LoadModelFromFile("ri");
+    zu2model = FbxLoader::GetInstance()->LoadModelFromFile("zu2");
+    mumodel = FbxLoader::GetInstance()->LoadModelFromFile("mu");
+
+    ba = new FBXobj3d();
+    ba->Initialize();
+    ba->SetPosition({ -5.0f,5.0f,5.0f });
+    ba->SetScale({ 0.005f,0.005f,0.005f });
+    ba->SetModel(bamodel);
+
+    nn = new FBXobj3d();
+    nn->Initialize();
+    nn->SetPosition({ -3.0f,5.0f,5.0f });
+    nn->SetScale({ 0.005f,0.005f,0.005f });
+    nn->SetModel(nnmodel);
+
+    zu = new FBXobj3d();
+    zu->Initialize();
+    zu->SetPosition({ -1.0f,5.0f,5.0f });
+    zu->SetScale({ 0.005f,0.005f,0.005f });
+    zu->SetModel(zumodel);
+
+    ri = new FBXobj3d();
+    ri->Initialize();
+    ri->SetPosition({ 1.0f,5.0f,5.0f });
+    ri->SetScale({ 0.005f,0.005f,0.005f });
+    ri->SetModel(rimodel);
+
+    zu2 = new FBXobj3d();
+    zu2->Initialize();
+    zu2->SetPosition({ 3.0f,5.0f,5.0f });
+    zu2->SetScale({ 0.005f,0.005f,0.005f });
+    zu2->SetModel(zu2model);
+
+    mu = new FBXobj3d();
+    mu->Initialize();
+    mu->SetPosition({ 5.0f,5.0f,5.0f });
+    mu->SetScale({ 0.005f,0.005f,0.005f });
+    mu->SetModel(mumodel);
+
+
     spriteCommon = new SpriteCommon();
     spriteCommon->Initialize(dxCommon->GetDevice(), dxCommon->GetCommandList(), Windows->window_width, Windows->window_height);
 
-    spriteCommon->LoadTexture(2, L"Resources/title.png");
+    spriteCommon->LoadTexture(2, L"Resources/start.png");
 
     title = Sprite::Create(spriteCommon, 2);
     title->SetPosition({ WindowsApp::window_width / 2,WindowsApp::window_height / 2,0 });
@@ -29,6 +85,18 @@ void TitleScene::Initialize(DXCommon* dxcommon, Input* input, Audio* audio, Spri
 
 void TitleScene::Update()
 {
+
+    //// マウスの入力を取得
+    //Input::MouseMove mouseMove = input->GetMouseMove();
+
+    //CurretmouseX = mouseMove.lX;
+    //CurretmouseY = mouseMove.lY;
+
+    //camera->SetmouseX(CurretmouseX);
+    //camera->SetmouseY(CurretmouseY);
+
+    camera->CurrentUpdate();
+
     if (titleflag == true)
     {
         if (spritesize.x >= 1280)
@@ -50,6 +118,13 @@ void TitleScene::Update()
             spritesize.x +=1.0f;
             spritesize.y +=1.0f;
         }
+
+        ba->Update();
+        nn->Update();
+        zu->Update();
+        ri->Update();
+        zu2->Update();
+        mu->Update();
 
         //title->SetRotation(spriteangle);
         title->SetSize(spritesize);
@@ -83,7 +158,14 @@ void TitleScene::Draw()
     if (titleflag == true)
     {
         title->Draw();
+        ba->Draw(cmdList);
+        nn->Draw(cmdList);
+        zu->Draw(cmdList);
+        ri->Draw(cmdList);
+        zu2->Draw(cmdList);
+        mu->Draw(cmdList);
     }
+
 }
 
 void TitleScene::Finalize()
--- Scene/TitleScene.h
+++ Scene/TitleScene.h
@@ -7,6 +7,11 @@
 #include "../2d/Sprite.h"
 #include "../Engine/WindowsApp.h"
 #include "../Engine/Audio.h"
+#include "../Engine/Camera.h"
+#include "fbxsdk.h"
+#include "../3d/FbxLoader.h"
+#include "../3d/FBXobj3d.h"
+#include "../3d/Modelfbx.h"
 #include <vector>
 
 
@@ -39,17 +44,41 @@ private:
 	Input* input = nullptr;
 	DXCommon* dxCommon = nullptr;
 	SpriteCommon* spriteCommon = nullptr;
+	Camera* camera = nullptr;
 
-
+	//sprite
 	Sprite* title = nullptr;
 	XMFLOAT3 spritepos{ WindowsApp::window_width / 2 ,WindowsApp::window_height / 2,0 };
 	XMFLOAT2 spritesize{ WindowsApp::window_width,WindowsApp::window_height };
 	XMFLOAT2 movesize = { 800,300 };
 
+	//3d
+	FbxModel* bamodel = nullptr;
+	FbxModel* nnmodel = nullptr;
+	FbxModel* zumodel = nullptr;
+	FbxModel* rimodel = nullptr;
+	FbxModel* zu2model = nullptr;
+	FbxModel* mumodel = nullptr;
+
+	FBXobj3d* ba = nullptr;
+	FBXobj3d* nn = nullptr;
+	FBXobj3d* zu = nullptr;
+	FBXobj3d* ri = nullptr;
+	FBXobj3d* zu2 = nullptr;
+	FBXobj3d* mu = nullptr;
+
+
 	bool transfrag = true;
 	float spriteangle = 0;
 	bool overfrag = true;
 	int wait = 0;
+
+	///カメラ関連
+	//ひとつ前のマウスの座標
+	float CurretmouseX = 0;
+	float CurretmouseY = 0;
+	//カメラ初期位置
+	XMFLOAT3 EyeInitialize = { 0,4,0 };
 
 	//今のシーンがタイトルかどうか
 	bool titleflag = true;
